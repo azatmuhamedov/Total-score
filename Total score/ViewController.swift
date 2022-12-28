721617a79777eb45d1111a5e548ba06faef5d4fb
azat <azotmuhamed@gmail.com>
2022-12-28 20:00:27 +0300

    Initial Commit

---
+++ Total score/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Total score
+//
+//  Created by Азат Мухамедов on 28.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
